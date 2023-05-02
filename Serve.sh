4d2bca5c1dd5372e24a60e021ea870469d33bf38
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2023-05-03 00:42:18 +0200

    [FEATURE] Configuration files added.

---
+++ Serve.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+status=$( docker container inspect -f '{{.State.Status}}' go-ascii )
+if [[ $status == "exited" ]]; then
+  echo "Starting container..."
+  docker start go-ascii
+  echo "Container started succesfully."
+elif [[ $status == "running" ]]; then
+  echo "Container already started"
+else
+  echo "Creating docker image..."
+  docker build -t go-ascii .
+  echo "Starting container..."
+  docker run --name go-ascii -d -p 8080:8080 go-ascii	
+  echo "Container started succesfully."
+fi
+$SHELL
---
+++ Stop.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+status=$( docker container inspect -f '{{.State.Status}}' go-ascii )
+if [[ $status == "exited" ]]; then
+  echo "Container already stopped."
+elif [[ $status == "running" ]]; then
+  echo "Stopping the container..."
+  docker stop go-ascii
+  echo "Container stopped succesfully."
+else
+  echo "Container does not exists."
+fi
+$SHELL
