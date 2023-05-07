e566c9c80f222fe2a5ac86643067d186e77d76b2
Rafael Ruiz <rafaelmostoles24595@gmail.com>
2023-05-07 04:47:55 +0200

    [FEATURE] Docker compose file updated.

---
+++ sql/create_tables.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE logdb;
+
+\c logdb;
+
+CREATE TABLE IF NOT EXISTS session (
+  session_id varchar(50) NOT NULL,
+  timestamp TIMESTAMP,
+  PRIMARY KEY (session_id)
+);
+
+CREATE TABLE IF NOT EXISTS register (
+  register_id SERIAL NOT NULL,
+  session_id_fk varchar(50) NOT NULL,
+  category varchar(30) NOT NULL,
+  message varchar(500) NOT NULL,
+  timestamp TIMESTAMP,
+  PRIMARY KEY (register_id),
+  CONSTRAINT session_id_fk
+      FOREIGN KEY(session_id_fk) 
+	  REFERENCES session(session_id)
+);
